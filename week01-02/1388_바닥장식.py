95c3641483227dbaa122f68a17fa49a924619abb
HyoungukJo <whguddnr9@gmail.com>
2024-07-19 13:14:16 +0900

    rebase directory

--- week01-02/1388_바닥장식.py
+++ week01-02/1388_바닥장식.py
@@ -1,10 +1,10 @@
-def dfs(start_x,start_y):
+# def dfs(start_x,start_y):
 
-    if depth ==
+    # if depth ==
 
 
-    for i in range(N):
-        if visited[start][a]==0, 
+    # for i in range(N):
+        # if visited[start][a]==0:
 
     
 N,M = map(int,input().split())
