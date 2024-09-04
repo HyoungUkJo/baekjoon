8bdb0ae43fe9062f11be23c3db3a3bece6fd4252
HyoungukJo <whguddnr9@gmail.com>
2024-09-04 10:02:21 +0900

    1932 solve

--- 하루하나씩/1932_정수삼각형.py
+++ 하루하나씩/1932_정수삼각형.py
@@ -1,16 +1,14 @@
 N = int(input())
+dp=[]
+for i in range(N):
+  dp.append(list(map(int, input().split())))
 
-arr = [list(map(int, input().split())) for _ in range(N)]
-dp = [[0]*N for _ in range(N)]
-
-dp[0]=arr[0]
 for i in range(1,N):
-    for j in range(i):
-        if j==0:
-            dp[i][j]=dp[i-1][j]+arr[i][j]
-        else :
-            dp[i][j]=dp[i-1][j]+max(arr[i][j],arr[i][j+1])
-        
-
-print(dp)
+  for j in range(len(dp[i])):
+    if j==0:
+      dp[i][j]=dp[i][j]+dp[i-1][j]
+    elif j==len(dp[i])-1: 
+      dp[i][j]=dp[i][j]+dp[i-1][j-1]
+    else:
+      dp[i][j]=max(dp[i-1][j-1],dp[i-1][j])+dp[i][j]
 print(max(dp[N-1]))
