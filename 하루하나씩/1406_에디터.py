cce5573b11a6401518d94b3cde4b7a242cc279b1
HyoungukJo <whguddnr9@gmail.com>
2024-11-05 10:32:40 +0900

    solve 1406

--- 하루하나씩/1406_에디터.py
+++ 하루하나씩/1406_에디터.py
@@ -1,42 +1,25 @@
-str = input()
-arr = list(str)
-max = len(arr)
-pointer = max
+import sys
 
-N = int(input())
+str1 = list(sys.stdin.readline().rstrip())
+str2 = []
 
+for _ in range(int(sys.stdin.readline())):
+    command = list(sys.stdin.readline().split())
 
-for _ in range(N) :
-    option = input().split(" ")
-    option = list(option)
-    # print("option:",option[0],"pointer:",pointer,"max:",max,"arr:",arr)
+    if command[0] == 'L':
+        if str1:
+            str2.append(str1.pop())
 
-    if option[0] == 'L':
-        if pointer==0:
-            continue
-        pointer -=1
-        
-    elif option[0] == 'D':    
-        if pointer == max+1:
-            continue
-        pointer +=1
+    elif command[0] == 'D':
+        if str2:
+            str1.append(str2.pop())
 
-    elif option[0] == 'B':
-        if pointer ==0:
-            continue
-        temp_arr = arr[pointer:len(arr)]
-        # print("temp",temp_arr)
-        # print("slice:",arr[0:pointer-1])
-        arr = arr[0:pointer-1]+temp_arr
-        max -=1
-        pointer-=1
+    elif command[0] == 'B':
+        if str1:
+            str1.pop()
 
-    elif option[0] == 'P':
-        arr.insert(pointer,option[1])
-        max +=1
-        pointer+=1
+    else:
+        str1.append(command[1])
 
-    else :
-        break
-
-print(*arr,sep="")
+str1.extend(reversed(str2))
+print(''.join(str1))
